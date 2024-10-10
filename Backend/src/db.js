0fa77117ebc142114fa5e1f845fda3576da85e75
Jaime <176935470+HolaRene@users.noreply.github.com>
2024-10-10 08:22:09 -0600

    algunos cambios

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -4,7 +4,7 @@ import { MONGODB_DATA } from "./config.js";
 export const conexionDb = async () => {
     try {
         await mongoose.connect(MONGODB_DATA)
-        console.log('>>> Se ha connectado la base de datos')
+        console.log('>>> Se ha connectado la base de datos', MONGODB_DATA)
     }
     catch (error) {
         console.log('Es es el error de la base de datos', error)
