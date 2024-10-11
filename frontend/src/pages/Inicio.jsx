f7d99bb3e1eebf6d7222496942b7b6cd4dca7693
Jaime <176935470+HolaRene@users.noreply.github.com>
2024-10-11 17:46:24 -0600

    se realizaron cambios en el frontend

--- frontend/src/pages/Inicio.jsx
+++ frontend/src/pages/Inicio.jsx
@@ -1,9 +1,31 @@
-import { useAuth } from '../context/AuthContext'
-
 function PerfilPage() {
   return (
     <div className='bg-zinc-800 h-[calc(100vh-100px)] flex flex-col justify-center items-center'>
-      ¡Registrate y guarda tus tareas!
+      <div className='text-center'>
+        <h1 className='text-white text-3xl mb-4'>
+          ¡Bienvenido a nuestra Aplicación Web!
+        </h1>
+        <p className='text-gray-400 mb-4'>
+          Esta es una aplicación web Fullstack que te permite registrarte,
+          guardar y eliminar tus tareas de manera sencilla.
+        </p>
+        <p className='text-gray-400 mb-4'>
+          Utilizamos Node.js como Backend y MongoDB como base de datos NoSQL,
+          junto con React como frontend.
+        </p>
+        <p className='text-gray-400'>
+          Además, podrás gestionar tus tareas usando JSON Web Tokens y cookies
+          para una experiencia segura.
+        </p>
+        <a
+          href='https://github.com/HolaRene/app-mongodb-tareas-crud' // Cambia esto por el enlace a tu repositorio
+          className='mt-4 text-blue-400 underline'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Ver el proyecto en GitHub
+        </a>
+      </div>
     </div>
   )
 }
